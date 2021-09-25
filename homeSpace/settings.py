f42256525f00924ed9b9d3d363e56f621d23b61a
Asliddin <69431423+AsliddinTuxtasinov@users.noreply.github.com>
2021-09-25 17:16:39 +0500

    update sittings.py file

--- homeSpace/settings.py
+++ homeSpace/settings.py
@@ -155,5 +155,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "tukhtasinovasliddin@gmail.com"
-EMAIL_HOST_PASSWORD = "asliddin1@!"
+EMAIL_HOST_USER = "your gmail "
+EMAIL_HOST_PASSWORD = "your password"
